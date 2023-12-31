fbcc477b61b1544200274bc349f6319a0f6437c2
Go, Christian Harrel Y. <chrome.go.christianharrel@gmail.com>
2023-12-31 20:51:25 +0800

    add realtime database to comment

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 const navLinks = document.querySelectorAll('nav ul li a');
+const commentBtn = document.querySelector('.comment-btn');
+const usernameInput = document.querySelector('#username');
+const commentInput = document.querySelector('#input_comment');
+const commentSection = document.querySelector('.comment-card');
+const sortSelect = document.querySelector('#sort_option');
+const api = 'https://harrel-comments-default-rtdb.asia-southeast1.firebasedatabase.app/comment.json';
+const initialComments = [];
+
+fetch(api)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        initialComments.push(data[key]);
+      });
+    }
+  })
+  .then(() => updateComments());
 
 const scrollToTarget = (targetId) => {
   const targetElement = document.querySelector(`#${targetId}`);
@@ -15,11 +33,8 @@ const markActiveLink = (activeLink) => {
 navLinks.forEach((link) => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
-
     const targetId = link.getAttribute('href').substring(1);
-
     scrollToTarget(targetId);
-
     markActiveLink(link);
   });
 });
@@ -49,25 +64,52 @@ document.addEventListener('click', (event) => {
   if (popupId) togglePopup(popupId);
 });
 
-const usernameInput = document.querySelector('#username');
-const commentInput = document.querySelector('#input_comment');
-const commentButton = document.querySelector('.comment-btn');
-const commentSection = document.querySelector('.comment-card');
-
-usernameInput.addEventListener('input', toggleButtonState);
+commentBtn.addEventListener('click', addComment);
 commentInput.addEventListener('input', toggleButtonState);
-commentButton.addEventListener('click', addComment);
+usernameInput.addEventListener('input', toggleButtonState);
+sortSelect.addEventListener('change', updateComments);
 
 function toggleButtonState() {
   const isInputValid = usernameInput.value.trim() && commentInput.value.trim();
-  commentButton.classList.toggle('active', isInputValid);
-  commentButton.disabled = !isInputValid;
+  commentBtn.classList.toggle('active', isInputValid);
+  commentBtn.disabled = !isInputValid;
 }
 
 function addComment() {
   const username = usernameInput.value.trim();
   const commentText = commentInput.value.trim();
 
+  if (!(username && commentText)) return;
+
+  const currentDate = new Date();
+  const commentDate = currentDate.toISOString();
+
+  const newComment = {
+    name: username,
+    comment: commentText,
+    date: commentDate
+  };
+
+  fetch(api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newComment)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      getRecentComment();
+    });
+
+  usernameInput.value = '';
+  commentInput.value = '';
+  toggleButtonState();
+
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+function toggleComment(username, commentText) {
   if (!(username && commentText)) return;
 
   const timestamp = new Date();
@@ -92,24 +134,52 @@ function addComment() {
 
   usernameInput.value = '';
   commentInput.value = '';
-  sortComments();
-  toggleButtonState();
 }
 
-function sortComments() {
-  const sortOption = document.querySelector('#sort_option').value;
-  const comments = Array.from(
-    commentSection.querySelectorAll('.comment-info')
-  );
+function getRecentComment() {
+  fetch(api)
+    .then((data) => data.json())
+    .then((response) => {
+      if (typeof response === 'object' && response !== null) {
+        const keys = Object.keys(response);
+        const lastKey = keys[keys.length - 1];
+        const lastObject = response[lastKey];
 
-  comments.sort((a, b) => {
-    const dateA = parseCommentTimestamp(a);
-    const dateB = parseCommentTimestamp(b);
-    return sortOption === 'newest' ? dateB - dateA : dateA - dateB;
-  });
+        const comment = lastObject.comment;
+        const name = lastObject.name;
+        initialComments.push(lastObject);
+        updateComments();
+      } else {
+        console.log('Response is not a JSON object or is null');
+      }
+    })
+    .catch((error) => console.error('Error fetching data:', error));
+}
 
+function updateComments() {
   commentSection.innerHTML = '';
-  comments.forEach((comment) => commentSection.appendChild(comment));
+  const allComments = [...initialComments];
+  const sortedComments = sortComments(initialComments, sortSelect.value);
+  sortedComments.forEach((comment) => {
+    const newComment = document.createElement('div');
+    newComment.classList.add('comment-info');
+    newComment.innerHTML = `
+        <p class="name">
+          ${comment.name} - ${formatDate(comment.date)}
+        </p>
+        <p class="comment">${comment.comment}</p>
+    `;
+    commentSection.appendChild(newComment);
+  });
+}
+
+function sortComments(comments, order) {
+  return comments.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+
+    return order === 'newest' ? dateB - dateA : dateA - dateB;
+  });
 }
 
 function parseCommentTimestamp(commentElement) {
@@ -117,4 +187,15 @@ function parseCommentTimestamp(commentElement) {
     .querySelector('.name')
     .textContent.split(' - ')[1];
   return new Date(timestampText);
+}
+
+function formatDate(dateString) {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  return new Date(dateString).toLocaleDateString(undefined, options);
 }
